7292fd26529a05f11bdf7298169a713173c3b6ba
kossy3 <ykoshi873@gmail.com>
2026-09-24 12:45:02 +0900

    試行錯誤

---
+++ assets/scripts/Meme.ts
@@ -0,0 +1,17 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Meme')
+export class Meme extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    
+}
+
+
